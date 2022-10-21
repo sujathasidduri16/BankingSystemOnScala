685f52c374c59312120545b6ff9d57b399202d15
sujathasidduri16 <sujathasidduri16@gmail.com>
2022-10-21 12:03:21 +0100

    committed the changes

--- src/main/scala/BankingSystem/CustomerLoginDetails.scala
+++ src/main/scala/BankingSystem/CustomerLoginDetails.scala
@@ -1,16 +1,34 @@
 package BankingSystem
-import scalikejdbc._
 
+// In this class we can storing the deatails user name and password by using the ListBuffer
+//and giving the mail account for user
 import scala.collection.mutable.ListBuffer
 class CustomerLoginDetails {
 
   // This class is used to maintain logged users details
 
   val username = new ListBuffer[String]()
- username += ("Sujatha1", "Suma1", "Dhruv1","Sravan1","Sravanthi1","Prasad1")
+  username += ("Sujatha1", "Suma1", "Dhruv1", "Sravan1", "Sravanthi1", "Prasad1")
   val password = new ListBuffer[String]()
   password += ("Sujatha123", "Suma123", "Dhruv123", "Sravan123", "Sravanthi123", "Prasad123")
 
-}
+  def main(args: Array[String]): Unit = {
+    println(apply("Sujatha", "gmail.com"))
+    println(unApply(apply("Sujatha", "gmail.com")))
+    println(unApply("Sujatha Sidduri"))
+  }
 
+  def apply(user: String, domain: String): String = {
+    user + "@" + domain
+  }
+
+  def unApply(str: String): Option[(String, String)] = {
+    val parts = str.split("@")
+    if (parts.length == 2) {
+      Some(parts(0), parts(1))
+    } else {
+      None
+    }
+  }
+}
 
